ad525eecada5cdd9a4d08f70a8561f321b4e60b8
Jehyeon Park <super3714@naver.com>
2022-03-19 02:24:49 +0900

    fourth commit

--- kudos_foot_odom/kudos_foot_odom/include/kudos_foot_odom/foot_odom_node.h
+++ kudos_foot_odom/kudos_foot_odom/include/kudos_foot_odom/foot_odom_node.h
@@ -1,3 +1,6 @@
+#ifndef FOOT_ODOM_NODE_H
+#define FOOT_ODOM_NODE_H
+
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "std_msgs/Int32.h"
@@ -7,18 +10,13 @@
 
 #define RAD2DEG 180/M_PI
 
-class foot_odom{
-public:
-  enum
-  {
-    go_straight= 0,
-    rotate_with_ball = 1,
-    rotate_without_ball = 2,
-    enable_odom = 3,
-  };
+namspace robotis_op
+{
 
-  foot_odom();
-  ~foot_odom();
+class foot_odom_node{
+public:
+  foot_odom_node();
+  ~foot_odom_node();
 
   double x_move_amplitude;
   double y_move_amplitude;
@@ -27,34 +25,42 @@ public:
   double position_x;
   double position_y;
   double orientation_yaw;
-
-  int step_num;
+  double period_time;
 
   int i;
   int j;
   int rotate_count;
+  int step_num;
 
   bool walking_is_start;
-  // bool standup_state; 
+  int walkingstate;
   double error;
   void walkingParamCallback(const op3_walking_module_msgs::WalkingParam::ConstPtr& msg);
   void walkingCommandCallback(const std_msgs::String::ConstPtr& msg);
 
   double errorfunction(double position_y, double orientation_yaw)
-  // void walkingStateCallback(const std_msgs::String::ConstPtr& msg);
 
 protected:
-  int walkingstate;
+  enum
+  {
+    go_straight= 0,
+    rotate_without_ball = 1,
+    enable_odom = 2,
+  };
+
   ros::NodeHandle nh;
 
   ros::Rate loop_rate;
 
   ros::Publisher x_pub;
   ros::Publisher y_pub;
-  ros::Publisher yaw_pub];
+  ros::Publisher yaw_pub;
   ros::Publisher step_num_pub;
+  ros::Publisher period_time_pub;
 
   ros::Subscriber walking_command_sub;
   ros::Subscriber sub;
-  // ros::Subscriber standup_state_sub = nh.subscribe("/robotis/walking/standup", 1000, walkingStateCallback); 
 };
+}
+
+#endif /* FOOT_ODOM_NODE_H */
--- kudos_foot_odom/kudos_foot_odom/src/foot_odom_node.cpp
+++ kudos_foot_odom/kudos_foot_odom/src/foot_odom_node.cpp
@@ -1,34 +1,42 @@
-// #include "foot_odom_node.h"
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Int32.h"
-#include "std_msgs/Float64.h"
-#include "op3_walking_module_msgs/WalkingParam.h"
-#include "math.h"
+#include "foot_odom_node.h"
+//namespace robotis_op
+//{
+// #include "ros/ros.h"
+// #include "std_msgs/String.h"
+// #include "std_msgs/Int32.h"
+// #include "std_msgs/Float64.h"
+// #include "op3_walking_module_msgs/WalkingParam.h"
+// #include "math.h"
 
-double x_move_amplitude=0;
-double y_move_amplitude=0;
-double angle_move_amplitude=0;
+// #define RAD2DEG 180/M_PI
+
+// SoccerDemo::SoccerDemo()
+  // : FALL_FORWARD_LIMIT(70),
+
+
+double x_move_amplitude = 0.0;
+double y_move_amplitude = 0.0;
+double angle_move_amplitude = 0.0;
 
 double position_x = 0.0;
 double position_y = 0.0;
 double orientation_yaw = 0.0;
-int step_num = 0;
+double period_time = 0.0;
 
 int i = 1;
 int j = 1;
+int step_num = 0;
 int rotate_count = 0;
 
 bool walking_is_start = false;
 
 const int go_straight = 0;
-const int rotate_with_ball = 1;
-const int rotate_without_ball = 2;
-const int enable_odom = 3;
+const int rotate_without_ball = 1;
+const int enable_odom = 2;
 
 int walkingstate = enable_odom;
+double error = 0.0;
 
-double error = 0;
 void walkingParamCallback(const op3_walking_module_msgs::WalkingParam::ConstPtr& msg)
 {
     x_move_amplitude = msg->x_move_amplitude;
@@ -36,6 +44,7 @@ void walkingParamCallback(const op3_walking_module_msgs::WalkingParam::ConstPtr&
       x_move_amplitude *= -1;
     y_move_amplitude = msg->y_move_amplitude;
     angle_move_amplitude = msg->angle_move_amplitude;
+    period_time = msg->period_time;
 }
 
 void walkingCommandCallback(const std_msgs::String::ConstPtr& msg)
@@ -50,14 +59,6 @@ void walkingCommandCallback(const std_msgs::String::ConstPtr& msg)
   }
 }
 
-double errorfunction(double position_y, double orientation_yaw){
-  if( tan(orientation_yaw) != 0)
-    position_x = position_y / tan(orientation_yaw);
-  else  
-    ROS_INFO("tan(orientation = 0");
-  return position_x;
-}
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "foot_odom_node"); // node name 
@@ -67,37 +68,41 @@ int main(int argc, char **argv)
   ros::Publisher y_pub = nh.advertise<std_msgs::Float64>("kubot_Pose/position_y", 1000);
   ros::Publisher yaw_pub = nh.advertise<std_msgs::Float64>("kubot_Pose/orientation_yaw", 1000);
   ros::Publisher step_num_pub = nh.advertise<std_msgs::Int32>("kubot_Pose/step_num", 1000);
+  ros::Publisher period_time_pub = nh.advertise<std_msgs::Float64>("kubot_Pose/period_time", 1000);
 
   ros::Subscriber walking_command_sub = nh.subscribe("/robotis/walking/command", 0, walkingCommandCallback);
   ros::Subscriber sub = nh.subscribe("/robotis/walking/set_params", 1000, walkingParamCallback);
   
+  // period_time : 600ms = 0.6s
+  // 1/period_time : 1/0.6 = 1.667Hz
   ros::Rate loop_rate(1);
+
   while (ros::ok())
   {
     std_msgs::Float64 x_msg;
     std_msgs::Float64 y_msg;
     std_msgs::Float64 yaw_msg;
     std_msgs::Int32 step_num_msg;
+    std_msgs::Float64 period_time_msg;
 
     step_num++;
     
-    //define walkingstate
-    if( (x_move_amplitude != 0) && (y_move_amplitude == 0) && (angle_move_amplitude == 0) )
+    //define walkingstate --> relative angle
+    if((x_move_amplitude != 0) && (y_move_amplitude == 0) && (angle_move_amplitude == 0))
       walkingstate = go_straight;
-    else if( (x_move_amplitude != 0) && (y_move_amplitude != 0) && (angle_move_amplitude != 0) )
-      walkingstate = rotate_with_ball;
-    else if( (x_move_amplitude != 0) && (y_move_amplitude == 0) && (angle_move_amplitude != 0) )
+    else if
+      ((x_move_amplitude != 0) && (y_move_amplitude == 0) && (angle_move_amplitude != 0))
       walkingstate = rotate_without_ball;
     else  
       walkingstate = enable_odom;
-
+    
     //calculation rotate count
     orientation_yaw = orientation_yaw + angle_move_amplitude;
     if(orientation_yaw >= 2 * i * M_PI)
     {
       i++;
       rotate_count++;
-      ROS_INFO(" rotate [%d] times", rotate_count);
+      ROS_INFO("rotate [%d] times", rotate_count);
     }
 
     // odometry
@@ -106,29 +111,29 @@ int main(int argc, char **argv)
       case go_straight:
         ROS_INFO("go straight");
 
-        if(j<=3)
+          if(j<=3)
         {
           position_x = position_x + (0.002/3);
           j++;
         }
         else
-          position_x = position_x + x_move_amplitude * cos(orientation_yaw) * 1.25; 
-
-        position_y += position_y;
-        break;
+          position_x = position_x + x_move_amplitude * cos(orientation_yaw) * 1.25; // correction value 
         
-      case rotate_with_ball: 
-        ROS_INFO("rotate with ball");
-        position_x = position_x + x_move_amplitude * cos(orientation_yaw);
-        position_y = position_y + x_move_amplitude * sin(orientation_yaw);
-        error = position_x - errorfunction(position_y, orientation_yaw);
-        ROS_INFO("error : %lf", error);
+        position_y += position_y;
+        
+        error = abs(position_x - step_num * 0.004); // 1step -> 0.004m
+        ROS_INFO("current_x_error : %lf", error);
+        
         break;
 
       case rotate_without_ball: 
         ROS_INFO("rotate without ball");
         position_x = position_x + x_move_amplitude * cos(orientation_yaw);
         position_y += position_y;
+
+        error = abs(orientation_yaw - step_num * 0.348888); // 18step -> 1rotate
+        ROS_INFO("current_yaw_error : %lf", error);
+        
         break;
 
       case enable_odom:
@@ -143,16 +148,18 @@ int main(int argc, char **argv)
     y_msg.data = position_y;
     yaw_msg.data = orientation_yaw * RAD2DEG;
     step_num_msg.data = step_num;
+    period_time_msg.data = period_time;
 
     x_pub.publish(x_msg);
     y_pub.publish(y_msg);
     yaw_pub.publish(yaw_msg);
+    step_num_pub.publish(step_num_msg);
+    period_time_pub.publish(period_time_msg);
 
     ROS_INFO("position_x : [%lf] centi meter", position_x * 1000);
     ROS_INFO("position_y : [%lf] centi meter", position_y * 1000);
     ROS_INFO("orientation_yaw : [%lf] degree", orientation_yaw * RAD2DEG);
-
-    step_num_pub.publish(step_num_msg);
+    ROS_INFO("=================================");
 
     ros::spinOnce();
 
@@ -163,3 +170,4 @@ int main(int argc, char **argv)
 
   return 0;
 }
+//}
